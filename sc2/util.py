368d8c210a2628e6c72278e0f899cf4cbf77c919
Hannes Karppila <hannes.karppila@gmail.com>
2017-08-20 17:35:10 +0300

    Add non-exact name comparison

--- sc2/util.py
+++ sc2/util.py
@@ -1,5 +1,8 @@
 def name_normalize(name):
     return name.replace(" ", "").lower()
 
-def name_matches(name1, name2):
-    return name_normalize(name1) == name_normalize(name2)
+def name_matches(name1, name2, exact=True):
+    n1 = name_normalize(name1)
+    n2 = name_normalize(name2)
+
+    return n1 == n2 or (not exact and (n1.startswith(n2) or n2.startswith(n1)))
