25fb55ed7d90834d36f0f536f4324facbb5db710
Hannes Karppila <hannes.karppila@gmail.com>
2017-09-01 02:49:16 +0300

    Add TvZ Human vs AI example

---
+++ examples/play_tvz.py
@@ -0,0 +1,14 @@
+import sc2
+from sc2 import Race
+from sc2.player import Human, Bot
+
+from zerg_rush import ZergRushBot
+
+def main():
+    sc2.run_game(sc2.maps.get("Abyssal Reef LE"), [
+        Human(Race.Terran),
+        Bot(Race.Zerg, ZergRushBot())
+    ], realtime=True)
+
+if __name__ == '__main__':
+    main()
