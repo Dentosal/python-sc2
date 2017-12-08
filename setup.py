5f17e36e11bf1fb161d8c0f9c421e784e9e4ec2e
Hannes Karppila <hannes.karppila@gmail.com>
2017-12-08 07:59:51 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "sc2",
     packages = ["sc2"],
-    version = "0.2.1",
+    version = "0.3.0",
     description = "A StarCraft II API Client for Python 3",
     license="MIT",
     author = "Hannes Karppila",
