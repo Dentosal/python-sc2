499479ab51a8c7649dc9318ca294795f9643861b
Hannes Karppila <hannes.karppila@gmail.com>
2018-01-18 13:30:39 +0200

    Set up logger

--- sc2/__init__.py
+++ sc2/__init__.py
@@ -9,6 +9,11 @@ def is_submodule(path):
 
 __all__ = [p.stem for p in Path(__file__).parent.iterdir() if is_submodule(p)]
 
+import sys, logging
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 from . import maps
 from .data import *
 from .bot_ai import BotAI
