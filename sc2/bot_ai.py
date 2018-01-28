a34faa9143e0535ce553c266d2aabef77b4fda6c
Reynald Pader <rey.pader@paymaya.com>
2018-01-28 19:57:17 +0800

    Wrap the response of `BotAI.can_afford` to return a wrapper
    Fixes #22

--- sc2/bot_ai.py
+++ sc2/bot_ai.py
@@ -110,7 +110,7 @@ class BotAI(object):
         else:
             cost = self._game_data.calculate_ability_cost(item_id)
 
-        return cost.minerals <= self.minerals and cost.vespene <= self.vespene
+        return CanAffordWrapper(cost.minerals <= self.minerals, cost.vespene <= self.vespene)
 
     def select_build_worker(self, pos, force=False):
         workers = self.workers.closer_than(20, pos) or self.workers
@@ -222,3 +222,13 @@ class BotAI(object):
 
     async def on_step(self, do, state, game_loop):
         raise NotImplementedError
+
+
+class CanAffordWrapper(object):
+
+    def __init__(self, can_afford_minerals, can_afford_vespene):
+        self.can_afford_minerals = can_afford_minerals
+        self.can_afford_vespene = can_afford_vespene
+
+    def __bool__(self):
+        return self.can_afford_minerals and self.can_afford_vespene
