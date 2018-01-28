c05f930e06d10603ce668a4e5b934ed66e67505c
Reynald Pader <rey.pader@paymaya.com>
2018-01-28 11:26:21 +0800

    Add auto-add-supply feature to build order

--- sc2/build_orders/build_order.py
+++ sc2/build_orders/build_order.py
@@ -26,34 +26,42 @@ class Intent(object):
 
 
 class BuildOrder(object):
-    def __init__(self, bot, build, worker_count=0):
+    def __init__(self, bot, build, worker_count=0, auto_add_supply=True):
         self.build = build
         self.bot = bot
         self.worker_count = worker_count
+        self.auto_add_supply = auto_add_supply
 
     async def execute_build(self, state):
+        bot = self.bot
+        if bot.supply_left <= (bot.supply_cap / 50) and not bot.already_pending(bot.supply_type)\
+                and self.auto_add_supply:
+            return await add_supply().execute(bot, state)
+
         for index, item in enumerate(self.build):
             condition, intent = item
             condition = item[0] if item[0] else always_true
-            if condition(self.bot, state) and not intent.is_done:
-                e = await intent.execute(self.bot, state)
+            if condition(bot, state) and not intent.is_done:
+                e = await intent.execute(bot, state)
                 if intent.is_done:
                     return e
                 else:
+                    if e == ActionResult.NotEnoughFood and self.auto_add_supply \
+                            and not bot.already_pending(bot.supply_type):
+                        return await add_supply().execute(bot, state)
                     continue
 
-        if self.bot.workers.amount < self.worker_count:
-            if self.bot.race == Race.Zerg:
-                return await morph(race_worker[Race.Zerg]).execute(self.bot, state)
+        if bot.workers.amount < self.worker_count:
+            if bot.race == Race.Zerg:
+                return await morph(race_worker[Race.Zerg]).execute(bot, state)
             else:
-                return await train(race_worker[self.bot.race], self.bot.townhalls.ready.random.type_id).execute(self.bot,
-                                                                                                         state)
+                return await train(race_worker[bot.race], race_townhalls[self.bot.race]).execute(bot, state)
         return None
 
 
 def expand():
     async def expand_spec(bot, state):
-        building = bot.townhalls.first.type_id
+        building = bot.basic_townhall_type
         if bot.can_afford(building):
             return await bot.expand_now(building=building)
         else:
@@ -73,6 +81,7 @@ def train(unit, on_building):
             return ActionResult.Error
 
     return Intent(train_spec)
+
 
 def morph(unit):
     async def train_spec(bot, state):
@@ -105,3 +114,36 @@ def build(building, around_building=None, placement=None):
             return ActionResult.Error
 
     return Intent(build_spec)
+
+
+def add_supply():
+    async def supply_spec(bot, state):
+        if bot.can_afford(bot.supply_type):
+            if bot.race == Race.Zerg:
+                return await morph(bot.supply_type).execute(bot, state)
+            else:
+                return await build(bot.supply_type).execute(bot, state)
+        else:
+            return ActionResult.Error
+
+    return Intent(supply_spec)
+
+
+def add_gas():
+    async def add_gas_spec(bot, state):
+        for th in bot.townhalls:
+            for nexus in bot.units(th.type_id).ready:
+                vgs = state.vespene_geyser.closer_than(20.0, nexus)
+                for vg in vgs:
+                    if not bot.can_afford(UnitTypeId.EXTRACTOR):
+                        break
+
+                    worker = bot.select_build_worker(vg.position)
+                    if worker is None:
+                        break
+
+                    if not bot.units(UnitTypeId.EXTRACTOR).closer_than(1.0, vg).exists:
+                        return await bot.do(worker.build(UnitTypeId.EXTRACTOR, vg))
+        return ActionResult.Error
+
+    return Intent(add_gas_spec)
