9f1b4d95b60d944bdb11f380f745bae3de0da82d
Reynald Pader <reynaldmpader@gmail.com>
2018-02-06 16:40:00 +0800

    Use `stableid.json` to generate ID enums (#24)
    Use the installed StarCraft  2 stableid.json for ids

--- examples/zerg_rush.py
+++ examples/zerg_rush.py
@@ -33,8 +33,8 @@ class ZergRushBot(sc2.BotAI):
 
         for queen in self.units(QUEEN).idle:
             abilities = await self.get_available_abilities(queen)
-            if AbilityId.INJECTLARVA in abilities:
-                await self.do(queen(INJECTLARVA, hatchery))
+            if AbilityId.EFFECT_INJECTLARVA in abilities:
+                await self.do(queen(EFFECT_INJECTLARVA, hatchery))
 
         if self.vespene >= 100:
             sp = self.units(SPAWNINGPOOL).ready
