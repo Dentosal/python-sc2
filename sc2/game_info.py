ff4ff0250ae257c41b335d4f2b8b989cd79e3d5e
Hannes Karppila <hannes.karppila@gmail.com>
2018-02-23 13:46:49 +0200

    Add initial ramp map

--- sc2/game_info.py
+++ sc2/game_info.py
@@ -1,6 +1,32 @@
+from copy import deepcopy
+
 from .position import Point2, Size, Rect
 from .pixel_map import PixelMap
 from .player import Player
+
+def _pxmap_sub_scale(map1, map2, map3):
+    """ (map1 - map2) * map3 """
+
+    assert map1.width == map2.width and map1.height == map2.height
+
+    values = [map3[(x, y)] for x in range(map1.width) for y in range(map1.height) if map1[(x, y)] == 0 and map2[(x, y)] == 0]
+    limits = min(values), max(values)
+
+    result = deepcopy(map1)
+
+    for x in range(map1.width):
+        for y in range(map1.height):
+            old = map1[(x, y)]
+            new = map2[(x, y)]
+
+            if old == 0 and new == 0:
+                scaled = int((map3[(x, y)] - limits[0]) * 255) // (limits[1] - limits[0])
+                print(scaled)
+                result[(x, y)] = bytearray([scaled])
+            else:
+                result[(x, y)] = b"\x00"
+
+    return result
 
 class GameInfo(object):
     def __init__(self, proto):
@@ -10,6 +36,7 @@ class GameInfo(object):
         self.terrain_height = PixelMap(proto.start_raw.terrain_height)
         self.placement_grid = PixelMap(proto.start_raw.placement_grid)
         self.playable_area = Rect.from_proto(proto.start_raw.playable_area)
+        self.map_ramps = _pxmap_sub_scale(self.pathing_grid, self.placement_grid, self.terrain_height)
         self.player_races = {p.player_id: p.race_actual or p.race_requested for p in proto.player_info}
         self.start_locations = [Point2.from_proto(sl) for sl in proto.start_raw.start_locations]
 
