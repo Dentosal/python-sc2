954f85140bc56727a0cc4721f08580e542b78918
Frigerius <frigerius@frigerius.de>
2018-07-12 14:56:15 +0200

    fix misplaced bracket

--- sc2/client.py
+++ sc2/client.py
@@ -181,8 +181,8 @@ class Client(Protocol):
         assert len(units) > 0
         result = await self._execute(query=query_pb.RequestQuery(
             abilities=[query_pb.RequestQueryAvailableAbilities(
-                unit_tag=unit.tag) for unit in units]),
-            ignore_resource_requirements=ignore_resource_requirements
+                unit_tag=unit.tag) for unit in units],
+            ignore_resource_requirements=ignore_resource_requirements)
         )
         return [[AbilityId(a.ability_id) for a in b.abilities] for b in result.query.abilities]
 
