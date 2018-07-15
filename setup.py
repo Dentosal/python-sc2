d055869b82138c28636b988aeef9bb3dddbb592b
Hannes Karppila <hannes.karppila@gmail.com>
2018-07-15 22:48:28 +0300

    Upgrade to Python 3.7 and Pipenv

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, find_packages
 
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
+pfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pfile['packages'], r=False)
+test_requirements = convert_deps_to_pip(pfile['dev-packages'], r=False)
+
 setup(
     name = "sc2",
     packages = find_packages(),
@@ -10,7 +17,8 @@ setup(
     author_email = "hannes.karppila@gmail.com",
     url = "https://github.com/Dentosal/python-sc2",
     keywords = ["StarCraft", "StarCraft 2", "StarCraft II", "AI", "Bot"],
-    install_requires=["s2clientprotocol", "websockets", "portpicker", "async_timeout"],
+    setup_requires=["pipenv"],
+    install_requires=requirements,
     classifiers = [
         "Development Status :: 3 - Alpha",
 
@@ -26,5 +34,6 @@ setup(
 
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ]
 )
