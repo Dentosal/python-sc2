70435894afc76e213fff1ae4d7f54dc189b53136
BurnySc2 <gamingburny@gmail.com>
2018-07-23 00:31:56 +0200

    Add another UnitTypeId

--- examples/terran/mass_reaper.py
+++ examples/terran/mass_reaper.py
@@ -75,7 +75,7 @@ class MassReaperBot(sc2.BotAI):
 
         # make scvs until 18, usually you only need 1:1 mineral:gas ratio for reapers, but if you don't lose any then you will need additional depots (mule income should take care of that)
         # stop scv production when barracks is complete but we still have a command cender (priotize morphing to orbital command)
-        if self.can_afford(SCV) and self.supply_left > 0 and self.units(UnitTypeId.SCV).amount < 18 and (self.units(UnitTypeId.BARRACKS).ready.amount < 1 and self.units(UnitTypeId.COMMANDCENTER).idle.exists or self.units(UnitTypeId.ORBITALCOMMAND).idle.exists):
+        if self.can_afford(UnitTypeId.SCV) and self.supply_left > 0 and self.units(UnitTypeId.SCV).amount < 18 and (self.units(UnitTypeId.BARRACKS).ready.amount < 1 and self.units(UnitTypeId.COMMANDCENTER).idle.exists or self.units(UnitTypeId.ORBITALCOMMAND).idle.exists):
             for th in self.townhalls.idle:
                 self.combinedActions.append(th.train(UnitTypeId.SCV))
 
