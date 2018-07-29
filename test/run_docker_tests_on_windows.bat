e4ef90aa31060f2455f574bf60f279f919eb182e
BurnySc2 <gamingburny@gmail.com>
2018-07-30 00:25:26 +0200

    Add local testing

--- test/run_docker_tests_on_windows.bat
+++ test/run_docker_tests_on_windows.bat
@@ -3,6 +3,9 @@ docker rm my_test
 
 REM Move to same location where travis builds from, if this batch file is in pythons-sc2/test/
 cd ..
+REM For local testing:
+REM docker build -t test_image2 -f test/temp/Dockerfile .
+REM For live usage:
 docker build -t test_image -f test/Dockerfile .
 
 REM Run image
