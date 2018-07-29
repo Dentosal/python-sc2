90fc9a450ade022c981165257a5fa9a13f4cd182
BurnySc2 <gamingburny@gmail.com>
2018-07-29 15:48:28 +0200

    Fix tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-echo hello from run tests script
+echo Hello world test from run_tests script
 python -m pip install /root/python-sc2
 python /root/python-sc2/test/test_units.py
