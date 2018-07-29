5e220c24a931b9426ee6bee9676360389144e48f
BurnySc2 <gamingburny@gmail.com>
2018-07-29 17:08:12 +0200

    Remove echo

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 echo Hello world test from run_tests script
-echo ls
-echo ls /root/
-echo ls /root/python-sc2
-echo ls /root/python-sc2/test
+ls
+ls /root/
+ls /root/python-sc2
+ls /root/python-sc2/test
 python -m pip install /root/python-sc2
 python /root/python-sc2/test/test_units.py
