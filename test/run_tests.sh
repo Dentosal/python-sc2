eb1c4410548ecd6453b59858ab55e245ee07ffef
BurnySc2 <gamingburny@gmail.com>
2018-07-29 17:42:03 +0200

    Fix test

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -9,5 +9,5 @@ echo Files in root/python-sc2:
 ls /root/python-sc2
 echo Files in root/python-sc2/test:
 ls /root/python-sc2/test
-python -m pip install ~/root/python-sc2
-python ~/root/python-sc2/test/test_units.py
+python -m pip install /python-sc2
+python /python-sc2/test/test_units.py
