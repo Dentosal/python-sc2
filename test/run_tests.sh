e349536f6b7356e0437e19dcd8ca4cddc72921ad
BurnySc2 <gamingburny@gmail.com>
2018-07-29 18:00:26 +0200

    Turn test into a quick test

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,11 +5,11 @@ echo Hello world test from run_tests script
 echo Files:
 ls
 echo Files in root:
-ls /root/
+ls ~/root/
 echo Files in root/python-sc2:
-ls /root/python-sc2
+ls ~/root/python-sc2
 echo Files in root/python-sc2/test:
-ls /root/python-sc2/test
+ls ~/root/python-sc2/test
 
 python -m pip install ~/python-sc2
 python ~/python-sc2/test/test_units.py
