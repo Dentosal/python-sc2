c325bb8a926a107cbb0ceb540cd7cb83f215359b
BurnySc2 <gamingburny@gmail.com>
2018-07-29 17:58:42 +0200

    Fix test

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo Hello world test from run_tests script
+
 echo Files:
 ls
 echo Files in root:
@@ -9,5 +10,6 @@ echo Files in root/python-sc2:
 ls /root/python-sc2
 echo Files in root/python-sc2/test:
 ls /root/python-sc2/test
-python -m pip install /python-sc2
-python /python-sc2/test/test_units.py
+
+python -m pip install ~/python-sc2
+python ~/python-sc2/test/test_units.py
