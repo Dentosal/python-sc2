d7be53db47524d3adb51b00f3995ba5ae8a7b324
BurnySc2 <gamingburny@gmail.com>
2018-07-30 02:01:33 +0200

    Add all example bots

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -11,6 +11,22 @@ echo Current path
 pwd
 python -m pip install .
 
+# Run all example bots
+python /root/python-sc2/examples/protoss/cannon_rush.py
+python /root/python-sc2/examples/protoss/threebase_voidray.py
+python /root/python-sc2/examples/protoss/warpgate_push.py
+
+python /root/python-sc2/examples/terran/cyclone_push.py
+python /root/python-sc2/examples/terran/mass_reaper.py
+python /root/python-sc2/examples/terran/onebase_battlecruiser.py
+python /root/python-sc2/examples/terran/proxy_rax.py
+python /root/python-sc2/examples/terran/ramp_wall.py
+
+python /root/python-sc2/examples/zerg/hydralisk_push.py
+python /root/python-sc2/examples/zerg/onebase_broodlord.py
+python /root/python-sc2/examples/zerg/zerg_rush.py
+
+
 # Run tests
 python /root/python-sc2/test/test_units.py
 python /root/python-sc2/test/test_bot.py
