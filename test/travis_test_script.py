9c12f763c4284c7286e1c2c1f36ce029ba7aedd3
BurnySc2 <gamingburny@gmail.com>
2018-07-30 20:32:04 +0200

    Fix timeout

--- test/travis_test_script.py
+++ test/travis_test_script.py
@@ -17,13 +17,12 @@ if len(sys.argv) > 1:
             if i - 1 < retries:
                 print("Relaunching bot {}, retrying {}/{}".format(sys.argv[1], i+2, retries))
             continue
-        finally:
-            if result is None:
-                print("Exiting with exit code 5, error: Attempted to launch script {} timed out after {} seconds. Retry attempt number {}".format(sys.argv[1], timeout_time, i+1))
-                exit(5)
-            else:
-                # Bot was successfully run in timeout_time seconds
-                break
+
+    # Bot was not successfully run in time
+    if result is None:
+        print("Exiting with exit code 5, error: Attempted to launch script {} timed out after {} seconds. Retry attempt number {}".format(sys.argv[1], timeout_time, i+1))
+        exit(5)
+
 
     print_output: str = result.stdout.decode('utf-8')
     print(print_output)
