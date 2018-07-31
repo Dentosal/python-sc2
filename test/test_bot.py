f2ef2e24c15c29dfa78d07dbfabd86b696ad84e2
BurnySc2 <gamingburny@gmail.com>
2018-07-31 04:32:17 +0200

    Add test_bot and remove other tests

--- test/test_bot.py
+++ test/test_bot.py
@@ -5,106 +5,114 @@ from sc2 import Race, Difficulty
 from sc2.constants import *
 from sc2.player import Bot, Computer
 
+from sc2.position import Point2, Point3
+from sc2.units import Units
+from sc2.unit import Unit
+
+from sc2.ids.unit_typeid import UnitTypeId
+from sc2.ids.ability_id import AbilityId
+from sc2.ids.buff_id import BuffId
+from sc2.ids.upgrade_id import UpgradeId
+from sc2.ids.effect_id import EffectId
+
+
 class TestBot(sc2.BotAI):
     def __init__(self):
-        self.drone_counter = 0
-        self.extractor_started = False
-        self.spawning_pool_started = False
-        self.moved_workers_to_gas = False
-        self.moved_workers_from_gas = False
-        self.queeen_started = False
-        self.mboost_started = False
+        self.tests_target = 3
+        self.tests_completed = 0
         print("Test bot initialized")
+
 
     async def on_step(self, iteration):
         if iteration == 0:
             await self.chat_send("(glhf)")
+        if iteration == 1:
+            assert len(self.state.chat) >= 1, self.state.chat
 
-        if not self.units(HATCHERY).ready.exists:
-            for unit in self.workers | self.units(ZERGLING) | self.units(QUEEN):
-                await self.do(unit.attack(self.enemy_start_locations[0]))
-            return
+        # Tests at start:
+        if iteration == 5:
+            # No need to use try except as the travis test script checks for "Traceback "
+            # try:
+            await self.test_botai_properties()
+            await self.test_gamestate_static_variables()
+            await self.test_game_info_static_variables()
+            # except AssertionError as e:
+            #     print("Assertion error: {}".format(e))
+            #     exit(1000)
 
-        hatchery = self.units(HATCHERY).ready.first
-        larvae = self.units(LARVA)
+        """
+        TODO tests:
+        expansion locations, expand now, get next expnasion, distribute workers, owned expansions, can cast, select build worker, can place, find placement, already_pending, self build, do actions, chat send
+        """
 
-        target = self.known_enemy_structures.random_or(self.enemy_start_locations[0]).position
-        for zl in self.units(ZERGLING).idle:
-            await self.do(zl.attack(target))
+        # End when all tests successful
+        if self.tests_completed >= self.tests_target:
+            exit(0)
 
-        for queen in self.units(QUEEN).idle:
-            abilities = await self.get_available_abilities(queen)
-            if AbilityId.EFFECT_INJECTLARVA in abilities:
-                await self.do(queen(EFFECT_INJECTLARVA, hatchery))
 
-        if self.vespene >= 100:
-            sp = self.units(SPAWNINGPOOL).ready
-            if sp.exists and self.minerals >= 100 and not self.mboost_started:
-                await self.do(sp.first(RESEARCH_ZERGLINGMETABOLICBOOST))
-                self.mboost_started = True
+    # Test BotAI properties
+    async def test_botai_properties(self):
+        assert 1 <= self.player_id <= 2, self.player_id
+        assert self.race == Race.Terran, self.race
 
-            if not self.moved_workers_from_gas:
-                self.moved_workers_from_gas = True
-                for drone in self.workers:
-                    m = self.state.mineral_field.closer_than(10, drone.position)
-                    await self.do(drone.gather(m.random, queue=True))
+        assert 0 <= self.time <= 180, self.time
+        assert self.start_location == self.townhalls.random.position, (self.start_location, self.townhalls.random.position)
+        for loc in self.enemy_start_locations:
+            assert isinstance(loc, Point2), loc
+            assert loc.distance_to(self.start_location) > 20, (loc, self.start_location)
+        assert self.known_enemy_units.amount == 0, self.known_enemy_units
+        assert self.known_enemy_structures.amount == 0, self.known_enemy_structures
+        assert self.known_enemy_structures.amount <= self.known_enemy_units.amount
+        assert self.main_base_ramp.top_center.distance_to(self.start_location) < 30, self.main_base_ramp.top_center
+        assert len(await self.get_available_abilities(self.workers)) == self.workers.amount
+        # assert self.can_afford(UnitTypeId.SCV) == True, self.minerals why error
+        assert self.minerals >= 50, self.minerals
+        assert self.vespene == 0, self.vespene
+        assert self.supply_used == 12, self.supply_used
+        assert self.supply_cap == 15, self.supply_cap
+        assert self.supply_left == 3, self.supply_left
 
-        if self.supply_left < 2:
-            if self.can_afford(OVERLORD) and larvae.exists:
-                await self.do(larvae.random.train(OVERLORD))
+        self.tests_completed += 1
 
-        if self.units(SPAWNINGPOOL).ready.exists:
-            if larvae.exists and self.can_afford(ZERGLING):
-                await self.do(larvae.random.train(ZERGLING))
 
-        if self.units(EXTRACTOR).ready.exists and not self.moved_workers_to_gas:
-            self.moved_workers_to_gas = True
-            extractor = self.units(EXTRACTOR).first
-            for drone in self.workers.random_group_of(3):
-                await self.do(drone.gather(extractor))
+    # Test self.state variables
+    async def test_gamestate_static_variables(self):
+        assert len(self.state.actions) == 0, self.state.actions
+        assert len(self.state.action_errors) == 0, self.state.action_errors
+        assert len(self.state.chat) == 0, self.state.chat
+        assert self.state.game_loop > 0, self.state.game_loop
+        assert self.state.score.collection_rate_minerals >= 0, self.state.score.collection_rate_minerals
+        assert len(self.state.destructables) > 0, self.state.destructables
+        assert self.state.units.amount > 0, self.state.units
+        assert len(self.state.blips) == 0, self.state.blips
+        assert len(self.state.upgrades) == 0, self.state.upgrades
+        # TODO: actions, actionerrors, observation, chat, common, psionic matrix as protoss, score, abilities, blips, pixelmaps, dead units, effects, upgrades
 
-        if self.minerals > 500:
-            for d in range(4, 15):
-                pos = hatchery.position.to2.towards(self.game_info.map_center, d)
-                if await self.can_place(HATCHERY, pos):
-                    self.spawning_pool_started = True
-                    await self.do(self.workers.random.build(HATCHERY, pos))
-                    break
+        self.tests_completed += 1
 
-        if self.drone_counter < 3:
-            if self.can_afford(DRONE):
-                self.drone_counter += 1
-                await self.do(larvae.random.train(DRONE))
 
-        if not self.extractor_started:
-            if self.can_afford(EXTRACTOR):
-                drone = self.workers.random
-                target = self.state.vespene_geyser.closest_to(drone.position)
-                err = await self.do(drone.build(EXTRACTOR, target))
-                if not err:
-                    self.extractor_started = True
+    # Test self._game_info variables
+    async def test_game_info_static_variables(self):
+        assert len(self._game_info.players) == 2, self._game_info.players
+        assert len(self._game_info.map_ramps) >= 2, self._game_info.map_ramps
+        assert len(self._game_info.player_races) == 2, self._game_info.player_races
+        # TODO: map size, pixel maps, playable area, player races, start locations, map center
 
-        elif not self.spawning_pool_started:
-            if self.can_afford(SPAWNINGPOOL):
-                for d in range(4, 15):
-                    pos = hatchery.position.to2.towards(self.game_info.map_center, d)
-                    if await self.can_place(SPAWNINGPOOL, pos):
-                        drone = self.workers.closest_to(pos)
-                        err = await self.do(drone.build(SPAWNINGPOOL, pos))
-                        if not err:
-                            self.spawning_pool_started = True
-                            break
+        self.tests_completed += 1
 
-        elif not self.queeen_started and self.units(SPAWNINGPOOL).ready.exists:
-            if self.can_afford(QUEEN):
-                r = await self.do(hatchery.train(QUEEN))
-                if not r:
-                    self.queeen_started = True
+    # TODO:
+    # Test position.py
+    # Test unit.py
+    # Test units.py
+    # Test ramp building placement position
+
+
+
 
 def main():
     sc2.run_game(sc2.maps.get("(2)CatalystLE"), [
-        Bot(Race.Zerg, TestBot()),
-        Computer(Race.Terran, Difficulty.Medium)
+        Bot(Race.Terran, TestBot()),
+        Computer(Race.Zerg, Difficulty.Easy)
     ], realtime=False)
 
 if __name__ == '__main__':
--- test/travis_test_script.py
+++ test/travis_test_script.py
@@ -4,8 +4,8 @@ import sys, subprocess, time
 This script is made as a wrapper for sc2 bots to set a timeout to the bots (in case they cant find the last enemy structure or the game is ending in a draw)
 """
 
-retries = 10
-timeout_time = 3*60
+retries = 3
+timeout_time = 2*60
 
 
 
@@ -18,6 +18,7 @@ if len(sys.argv) > 1:
 
         process = subprocess.Popen(["python", sys.argv[1]], stdout=subprocess.PIPE)
         try:
+            # Stop the current bot if the timeout was reached - the bot needs to finish a game within 3 minutes real time
             result = process.communicate(timeout=timeout_time)
         except subprocess.TimeoutExpired:
             continue
@@ -27,6 +28,18 @@ if len(sys.argv) > 1:
         # Break as the bot run was successful
         break
 
+    if process.returncode is not None:
+        # Reformat the output into a list
+        print_output: str = result
+        linebreaks = [
+            ["\r\n", print_output.count("\r\n")],
+            ["\r", print_output.count("\r")],
+            ["\n", print_output.count("\n")],
+        ]
+        most_linebreaks_type = max(linebreaks, key=lambda x: x[1])
+        linebreak_type, linebreak_count = most_linebreaks_type
+        output_as_list = print_output.split(linebreak_type)
+        print("Output: {}".format(output_as_list))
 
     # Bot was not successfully run in time, returncode will be None
     if process.returncode is None or process.returncode != 0:
@@ -34,16 +47,6 @@ if len(sys.argv) > 1:
         exit(5)
 
 
-    # Reformat the output into a list
-    print_output: str = result
-    linebreaks = [
-        ["\r\n", print_output.count("\r\n")],
-        ["\r", print_output.count("\r")],
-        ["\n", print_output.count("\n")],
-    ]
-    most_linebreaks_type = max(linebreaks, key=lambda x: x[1])
-    linebreak_type, linebreak_count = most_linebreaks_type
-    output_as_list = print_output.split(linebreak_type)
 
 
     # process.returncode will always return 0 if the game was run successfully or if there was a python error (in this case it returns as defeat)
@@ -53,7 +56,7 @@ if len(sys.argv) > 1:
         for line in output_as_list:
             # This will throw an error if a bot is called Traceback
             if "Traceback " in line:
-                print("Exiting with exit code 3, error log:\r\n{}".format(output_as_list))
+                print("Exiting with exit code 3")
                 exit(3)
         print("Exiting with exit code 0")
         exit(0)
