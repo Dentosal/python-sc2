5f3a992dbc2243005e673ffa9b5669241233f49e
BurnySc2 <gamingburny@gmail.com>
2018-07-31 15:14:15 +0200

    Improve test_bot, remove other tests

--- test/travis_test_script.py
+++ test/travis_test_script.py
@@ -39,7 +39,7 @@ if len(sys.argv) > 1:
         most_linebreaks_type = max(linebreaks, key=lambda x: x[1])
         linebreak_type, linebreak_count = most_linebreaks_type
         output_as_list = print_output.split(linebreak_type)
-        print("Output: {}".format(output_as_list))
+        print("Travis test script, bot output:\r\n{}".format("\r\n".join(output_as_list)))
 
     # Bot was not successfully run in time, returncode will be None
     if process.returncode is None or process.returncode != 0:
