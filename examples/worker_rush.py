bd6bb3f939d1c5cd946a8b25813ee215e4483afd
Anssi Kinnunen <anssi.kinnunen@reaktor.com>
2018-12-03 15:52:38 +0200

    update worker_rush.py example

--- examples/worker_rush.py
+++ examples/worker_rush.py
@@ -4,9 +4,11 @@ from sc2.player import Bot, Computer
 
 class WorkerRushBot(sc2.BotAI):
     async def on_step(self, iteration):
+        actions = []
         if iteration == 0:
             for worker in self.workers:
-                await self.do(worker.attack(self.enemy_start_locations[0]))
+                actions.append(worker.attack(self.enemy_start_locations[0]))
+            await self.do_actions(actions)
 
 def main():
     run_game(maps.get("Abyssal Reef LE"), [
