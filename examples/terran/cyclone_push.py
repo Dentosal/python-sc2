0bf7f13949ce42d13c1aa822a65ee354ad9bbe06
Anssi Kinnunen <anssi.kinnunen@reaktor.com>
2018-12-03 16:00:28 +0200

    update cyclone_push.py example

--- examples/terran/cyclone_push.py
+++ examples/terran/cyclone_push.py
@@ -22,11 +22,14 @@ class ProxyRaxBot(sc2.BotAI):
         return self.state.mineral_field.random.position
 
     async def on_step(self, iteration):
+        actions = []
+
         cc = self.units(COMMANDCENTER)
         if not cc.exists:
             target = self.known_enemy_structures.random_or(self.enemy_start_locations[0]).position
             for unit in self.workers | self.units(CYCLONE):
-                await self.do(unit.attack(target))
+                actions.append(unit.attack(target))
+            await self.do_actions(actions)
             return
         else:
             cc = cc.first
@@ -37,13 +40,13 @@ class ProxyRaxBot(sc2.BotAI):
             forces = self.units(CYCLONE)
             if (iteration//50) % 10 == 0:
                 for unit in forces:
-                    await self.do(unit.attack(target))
+                    actions.append(unit.attack(target))
             else:
                 for unit in forces.idle:
-                    await self.do(unit.attack(target))
+                    actions.append(unit.attack(target))
 
         if self.can_afford(SCV) and self.workers.amount < 22 and cc.noqueue:
-            await self.do(cc.train(SCV))
+            actions.append(cc.train(SCV))
 
         elif self.supply_left < 3:
             if self.can_afford(SUPPLYDEPOT) and self.already_pending(SUPPLYDEPOT) < 2:
@@ -65,7 +68,7 @@ class ProxyRaxBot(sc2.BotAI):
                         if worker is None:
                             break
 
-                        await self.do(worker.build(REFINERY, vg))
+                        actions.append(worker.build(REFINERY, vg))
                         break
 
             if self.units(BARRACKS).ready.exists:
@@ -77,17 +80,19 @@ class ProxyRaxBot(sc2.BotAI):
         for factory in self.units(FACTORY).ready.noqueue:
             # Reactor allows us to build two at a time
             if self.can_afford(CYCLONE):
-                await self.do(factory.train(CYCLONE))
+                actions.append(factory.train(CYCLONE))
 
 
         for a in self.units(REFINERY):
             if a.assigned_harvesters < a.ideal_harvesters:
                 w = self.workers.closer_than(20, a)
                 if w.exists:
-                    await self.do(w.random.gather(a))
+                    actions.append(w.random.gather(a))
 
         for scv in self.units(SCV).idle:
-            await self.do(scv.gather(self.state.mineral_field.closest_to(cc)))
+            actions.append(scv.gather(self.state.mineral_field.closest_to(cc)))
+
+        await self.do_actions(actions)
 
 def main():
     sc2.run_game(sc2.maps.get("(2)CatalystLE"), [
