15a74ab059c257a7665e2d9a6f0471c12b2a4f82
FW <tweakimp@gmail.com>
2019-02-09 00:06:37 +0100

    Add zergling for exception

--- sc2/game_data.py
+++ sc2/game_data.py
@@ -1,12 +1,14 @@
 from bisect import bisect_left
 from functools import lru_cache, reduce
-from typing import List, Dict, Set, Tuple, Any, Optional, Union # mypy type checking
+from typing import Any, Dict, List, Optional, Set, Tuple, Union  # mypy type checking
 
+from .constants import ZERGLING
 from .data import Attribute, Race
 from .ids.ability_id import AbilityId
-from .ids.unit_typeid import UnitTypeId
 from .ids.effect_id import EffectId
+from .ids.unit_typeid import UnitTypeId
 from .unit_command import UnitCommand
+
 
 # Set of parts of names of abilities that have no cost
 # E.g every ability that has 'Hold' in its name is free
@@ -45,14 +47,10 @@ class GameData:
             if unit.creation_ability == ability:
                 if unit.id == ZERGLING:
                     # HARD CODED: zerglings are generated in pairs
-                    return Cost(
-                        unit.cost.minerals * 2,
-                        unit.cost.vespene * 2,
-                        unit.cost.time
-                    )
+                    return Cost(unit.cost.minerals * 2, unit.cost.vespene * 2, unit.cost.time)
                 # Correction for morphing units, e.g. orbital would return 550/0 instead of actual 150/0
                 morph_cost = unit.morph_cost
-                if morph_cost: # can be None
+                if morph_cost:  # can be None
                     return morph_cost
                 # Correction for zerg structures without morph: Extractor would return 75 instead of actual 25
                 return unit.cost_zerg_corrected
@@ -62,6 +60,7 @@ class GameData:
                 return upgrade.cost
 
         return Cost(0, 0)
+
 
 class AbilityData:
     ability_ids: List[int] = [ability_id.value for ability_id in AbilityId]  # sorted list
@@ -115,6 +114,7 @@ class AbilityData:
     @property
     def cost(self) -> "Cost":
         return self._game_data.calculate_ability_cost(self.id)
+
 
 class UnitTypeData:
     def __init__(self, game_data, proto):
@@ -200,11 +200,7 @@ class UnitTypeData:
 
     @property
     def cost(self) -> "Cost":
-        return Cost(
-            self._proto.mineral_cost,
-            self._proto.vespene_cost,
-            self._proto.build_time
-        )
+        return Cost(self._proto.mineral_cost, self._proto.vespene_cost, self._proto.build_time)
 
     @property
     def cost_zerg_corrected(self) -> "Cost":
@@ -213,11 +209,7 @@ class UnitTypeData:
             # a = self._game_data.units(UnitTypeId.ZERGLING)
             # print(a)
             # print(vars(a))
-            return Cost(
-                self._proto.mineral_cost - 50,
-                self._proto.vespene_cost,
-                self._proto.build_time
-            )
+            return Cost(self._proto.mineral_cost - 50, self._proto.vespene_cost, self._proto.build_time)
         else:
             return self.cost
 
@@ -228,13 +220,17 @@ class UnitTypeData:
         if self.tech_alias is None or self.tech_alias[0] in {UnitTypeId.TECHLAB, UnitTypeId.REACTOR}:
             return None
         # Morphing a HIVE would have HATCHERY and LAIR in the tech alias - now subtract HIVE cost from LAIR cost instead of from HATCHERY cost
-        tech_alias_cost_minerals = max([self._game_data.units[tech_alias.value].cost.minerals for tech_alias in self.tech_alias])
-        tech_alias_cost_vespene = max([self._game_data.units[tech_alias.value].cost.vespene for tech_alias in self.tech_alias])
+        tech_alias_cost_minerals = max(
+            [self._game_data.units[tech_alias.value].cost.minerals for tech_alias in self.tech_alias]
+        )
+        tech_alias_cost_vespene = max(
+            [self._game_data.units[tech_alias.value].cost.vespene for tech_alias in self.tech_alias]
+        )
         return Cost(
-                self._proto.mineral_cost - tech_alias_cost_minerals,
-                self._proto.vespene_cost - tech_alias_cost_vespene,
-                self._proto.build_time
-            )
+            self._proto.mineral_cost - tech_alias_cost_minerals,
+            self._proto.vespene_cost - tech_alias_cost_vespene,
+            self._proto.build_time,
+        )
 
 
 class UpgradeData:
