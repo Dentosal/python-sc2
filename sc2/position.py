81c1d029f8a41c9b607537f22cffba22d5c8c10b
FW <tweakimp@gmail.com>
2019-04-23 23:29:52 +0200

    Roll back to epsilon difference on position __eq__

--- sc2/position.py
+++ sc2/position.py
@@ -3,6 +3,8 @@ import math
 import random
 from typing import Any, Dict, List, Optional, Set, Tuple, Union  # for mypy type checking
 
+EPSILON = 10 ** -8
+
 
 def _sign(num):
     return math.copysign(1, num)
@@ -136,7 +138,7 @@ class Pointlike(tuple):
 
     def __eq__(self, other):
         try:
-            return all(a == b for a, b in itertools.zip_longest(self, other, fillvalue=0))
+            return all(abs(a - b) <= EPSILON for a, b in itertools.zip_longest(self, other, fillvalue=0))
         except:
             return False
 
